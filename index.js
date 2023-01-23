66828d46f5337c06f22fa557712711c0dbb440ad
thejastheju <47556723+thejastheju@users.noreply.github.com>
2023-01-23 10:00:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+function initMap() {
+  const myLatlng = { lat: 12.92648684917372, lng: 77.55797843376155 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: myLatlng,
+  });
+  // Create the initial InfoWindow.
+  let infoWindow = new google.maps.InfoWindow({
+    content: "Click the map to get Latitude and Longitude!",
+    position: myLatlng,
+  });
+
+  infoWindow.open(map);
+  // Configure the click listener.
+  map.addListener("click", (mapsMouseEvent) => {
+    // Close the current InfoWindow.
+    infoWindow.close();
+    // Create a new InfoWindow.
+    infoWindow = new google.maps.InfoWindow({
+      position: mapsMouseEvent.latLng,
+    });
+    infoWindow.setContent(
+      JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+    );
+    infoWindow.open(map);
+  });
+}
+
+window.initMap = initMap;
